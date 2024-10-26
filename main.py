14d73b5762478b6c8c1bf67cec7bc46ccdbf2339
Howard Wen <wen.howard2012@gmail.com>
2024-10-26 13:53:37 -0400

    integrated RTC

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import urequests
 import time
 import iglooh_bme280
 import iglooh_dbm
+import iglooh_rtc
 
 # CONSTANTS
 # SPI pins
@@ -23,7 +24,10 @@ I2C_SCL = 22
 WIFI_SSID = "BELL882"
 WIFI_PW = "37E26FF916A6"
 
-DEBUG = 1
+DEBUG = 0
+
+# VARIABLES
+wifi_connected = False
 
 # setup I2C and SPI
 spi = SPI(1, baudrate=1000000, polarity=0, phase=0, sck=Pin(SCK), mosi=Pin(MOSI), miso=Pin(MISO))
@@ -32,10 +36,12 @@ i2c = I2C(1, sda=Pin(I2C_SDA), scl=Pin(I2C_SCL), freq=10000)
 # initialize bme and dbm objects
 bme = iglooh_bme280.BME280_SPI(bme_cs=BME_CS, spi=spi)
 dbm = iglooh_dbm.DBM_I2C(i2c=i2c)
-rtc = None
+rtc = iglooh_rtc.RTC_I2C(i2c=i2c)
 
 # WiFi Setup
 def connect_wifi():
+    global wifi_connected
+
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(WIFI_SSID, WIFI_PW)
@@ -43,7 +49,9 @@ def connect_wifi():
     while not wlan.isconnected():
         print(f"Connecting to WiFi network {WIFI_SSID} ...")
         time.sleep(5)
+    
     print("Connected!")
+    wifi_connected = True
 
 def bme280_setup():
     # Setup the BME280 with default settings
@@ -57,18 +65,24 @@ def dbm_setup():
     else:
         print("error setting up dbm")
 
+def rtc_setup():
+    global wifi_connected
+    # Setup RTC
+    return rtc.setup(wifi_connected=wifi_connected)
+
 
 # Setup sensors
 def setup():
     # connect wifi
     connect_wifi() if not DEBUG else print("debug mode")
 
+    # SPI PERIPHERALS SETUP
     # set up BME280
     print("BME280 test (Using SPI connection)")
     bme280_setup()
     print("BME280 setup complete")
     
-    # set up I2C peripherals
+    # I2C PERIPHERALS SETUP
     i2c_sensors = i2c.scan()
     print(f"Connected I2C sensors: {i2c_sensors}")
 
@@ -78,8 +92,13 @@ def setup():
         dbm_setup()
         print("Sound Decibel Meter setup complete")
     # RTC setup
-    # if rtc.ADDR in i2c_sensors:
-    #     print("setting up RTC")
+    if rtc.ADDR in i2c_sensors:
+        print("setting up RTC")
+        time = rtc_setup()
+        if time:
+            print(f"set up RTC. Current time: {time}")
+        else:
+            print("Failed to setup RTC")
 
 # Main loop
 def main_loop():
@@ -89,6 +108,9 @@ def main_loop():
         # Read decibel values
         db_array = dbm.get_db()
         if db_array:
+            curr_time = rtc.get_time()
+            print(f"Current time: {curr_time}")
+            
             avg_db = db_array[0]
             min_db = db_array[1]
             max_db = db_array[2]
@@ -98,7 +120,7 @@ def main_loop():
             print(f"Temperature: {temperature:.2f} C, Pressure: {pressure:.2f} hPa, Humidity: {humidity:.2f}%")
 
             # Assemble JSON with dB SPL reading
-            DBMjson = f'{{"db": {avg_db}, "temperature": {temperature}}}'
+            DBMjson = f'{{"time": {curr_time}, "db": {avg_db}, "temperature": {temperature}}}'
             print("JSON: \n" + DBMjson)
 
             # Make a POST request with the data
